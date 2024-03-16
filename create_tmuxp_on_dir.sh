0e2257b1e6bc300834c74bd222a967d9574e127d
benjaminshawki <benjaminshawki@gmail.com>
2024-03-16 15:45:22 +0100

    refactor(tmuxp): Made it easier to adjust yml

--- create_tmuxp_on_dir.sh
+++ create_tmuxp_on_dir.sh
@@ -11,16 +11,22 @@ inotifywait -m -e create --format "%f" "$WORKSPACE_DIR" | while read DIRNAME
 do
     # Check if a directory was created
     if [[ -d "$WORKSPACE_DIR/$DIRNAME" ]]; then
-        # Create the tmuxp config file
+        # Create the tmuxp config file using a here-document for simplicity
         CONFIG_PATH="$TMUXP_CONFIG_DIR/$DIRNAME.yml"
-        echo "session_name: $DIRNAME" > "$CONFIG_PATH"
-        echo "start_directory: \$HOME/dev/$DIRNAME" >> "$CONFIG_PATH"
-        echo "" >> "$CONFIG_PATH"
-        echo "windows:" >> "$CONFIG_PATH"
-        echo "  - window_name: $DIRNAME" >> "$CONFIG_PATH"
-        echo "    layout: tiled" >> "$CONFIG_PATH"
-        echo "    panes:" >> "$CONFIG_PATH"
-        echo "      -" >> "$CONFIG_PATH"
+        cat > "$CONFIG_PATH" <<EOF
+session_name: $DIRNAME
+start_directory: \$HOME/dev/$DIRNAME
+
+windows:
+  - window_name: $DIRNAME
+    layout: tiled
+    panes:
+      -
+  - window_name: terms
+    layout: tiled
+    panes:
+      -
+EOF
         echo "tmuxp config for $DIRNAME created at $CONFIG_PATH"
     fi
 done
